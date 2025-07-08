fe6739e82509c3aefe81d849e4fe2d1cee4e3efa
Rohit0445 <meenarohit458@gmail.com>
2025-07-08 10:37:26 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,59 @@
 function Hero(){
     return(
         <>
-        <p>HEY THERE !</p>
+           <nav>
+            <h1>BREED</h1>
+            <ul>
+                <li>About</li>
+                <li>Protfolio</li>
+                <li>Pages</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+           </nav>
+
+
+
+
+           <div id="maindiv">
+             <div id="div1">
+            <p>HEY THERE !</p>
         <h1>I AM JO BREED</h1>
-        <p>CREATIVE ART DIRECTOR & DESIGNER</p> 
+        <h3>CREATIVE ART DIRECTOR & DESIGNER</h3>
 
         <button>SEE MY WORK</button>
         
+        </div>
+
+        <div id="div2">
+            <img src="man.png" alt="" />
+        </div>
+        
+        </div>
+
+        <div id="seconddiv">
+
+            <div id="seconddiv1">
+                <h1>15K+</h1>
+                <p>happy Customer</p>
+            </div>
+
+            <div id="seconddiv1">
+                <h1>12k+</h1>
+                <p>Ticket Solved</p>
+
+
+            </div>
+            <div id="seconddiv1">
+                 <h1>9/10</h1>
+                <p> Average Rating</p>
+
+            </div>
+        </div>
+
+        <div id="thirddiv">
+
+        </div>
         
         </>
     )
